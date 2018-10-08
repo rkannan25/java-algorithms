1ecd73dd0c053c0e25a42525d1d4335c7e7d2199
Bodlapati, Trivedi Kumar <trivedi.bodlapati@ge.com>
2018-10-07 22:23:31 -0500

    added key notes for recursive algo

--- data-structures/src/main/java/com/test/learning/recursive/Factorial.java
+++ data-structures/src/main/java/com/test/learning/recursive/Factorial.java
@@ -9,6 +9,18 @@
 
 package com.test.learning.recursive;
 
+/**
+ * Recursive algorithms have two types of cases, recursive cases and base cases.
+ * • Every recursive function case must terminate at a base case.
+ * • Generally, iterative solutions are more efficient than recursive solutions [due to the
+ * overhead of function calls].
+ * • A recursive algorithm can be implemented without recursive function calls using a
+ * stack, but it’s usually more trouble than its worth. That means any problem that can
+ * be solved recursively can also be solved iteratively.
+ * • For some problems, there are no obvious iterative algorithms.
+ * • Some problems are best suited for recursive solutions while others are not.
+ */
+
 public class Factorial {
     public static int find(int number) {
         if (number == 0) return 1;
