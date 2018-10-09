e2ee73fa9199bf68b5c30e2b4ddee552afcc1917
Bodlapati, Trivedi Kumar <trivedi.bodlapati@ge.com>
2018-10-08 22:29:23 -0500

    removed header def

--- data-structures/src/test/java/com/test/learning/recursive/FactorialTest.java
+++ data-structures/src/test/java/com/test/learning/recursive/FactorialTest.java
@@ -1,13 +1,5 @@
-/*
- * Copyright (c) 2018 GE Healthcare
- * All Rights Reserved.
- * No portions of this source code or the resulting compiled program
- * may be used without written permission of GE Healthcare, Inc
- * or beyond the terms and conditions stipulated in the agreement/contract
- * under which the software has been supplied.
- */
-
 package com.test.learning.recursive;
+
 import org.junit.Assert;
 import org.junit.Test;
 
